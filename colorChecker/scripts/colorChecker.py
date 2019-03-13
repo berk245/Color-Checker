e303ed982610e59e686177af58f3c024378ae9df
unknown <berkozzambak@gmail.com>
2019-03-13 13:46:51 +0100

    color checker file

---
+++ colorChecker/scripts/colorChecker.py
@@ -0,0 +1,15 @@
+import cgi
+
+form = cgi.FieldStorage()
+
+user_color = form.getvalue('color')
+
+#data from the immense file somehow
+
+#def checkItOut(color):
+    #if the color provided is a string
+        #
+
+    #if it's a hex code
+
+    #if it's neither
